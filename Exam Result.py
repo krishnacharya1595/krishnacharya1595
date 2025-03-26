5649c2c2b91adf552652a0bfddf6a9e5c695f448
krishnacharya1595 <160568054+krishnacharya1595@users.noreply.github.com>
2025-03-26 11:51:41 +0530

    Add files via upload

---
+++ Exam Result.py
@@ -0,0 +1,15 @@
+print("Good Morning")
+
+MarksObtained = int(input("What is your highest marks ?"))
+if MarksObtained >= 85:
+    print("Distinction")
+    print("All the best")
+elif MarksObtained >=60 and MarksObtained <85:
+    print("First Class")
+elif MarksObtained >=35 and MarksObtained <60:
+    print("Congrats, You have passed")
+else:
+    print("Sorry, You have failed")
+
+
+
